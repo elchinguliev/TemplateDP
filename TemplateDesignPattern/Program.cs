171de0c1a12375f401354bde97d387f0c42cd698
elchinguliev <elchingulievv@gmail.com>
2023-04-13 02:14:22 +0400

    Add project files.

---
+++ TemplateDesignPattern/Program.cs
@@ -0,0 +1,66 @@
+﻿public enum PaymentType
+{
+    Cash,Credit   
+}
+public abstract class Shopping
+{
+    protected string ProductName;
+    protected PaymentType paymentType;
+    public void Start()
+    {
+        Console.WriteLine("Shopping started ");
+    }
+    public void End()
+    {
+        Console.WriteLine($"Shopping ended. {ProductName} is gained by   {paymentType} method");
+    }
+    abstract public void Product();
+    abstract public void PaymentMethod();
+    public void TemplateMethod()
+    {
+        Start();
+        Product();
+        PaymentMethod();
+        End();
+    }
+}
+class TV : Shopping
+{
+    public override void PaymentMethod()
+    {
+       paymentType = PaymentType.Cash;
+    }
+
+    public override void Product()
+    {
+        ProductName="TV";
+    }
+}
+
+class Refrigator : Shopping
+{
+    public override void PaymentMethod()
+    {
+        paymentType = PaymentType.Credit;
+    }
+
+    public override void Product()
+    {
+        ProductName="Refrigator";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+
+        Shopping sh1 = new TV();
+        sh1.TemplateMethod();
+        Console.WriteLine("******");
+        Shopping sh2 = new Refrigator();
+        sh2.TemplateMethod();
+        Console.Read();
+    }
+}
